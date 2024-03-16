a1565150d134743601fe63a26dc9a85e38765464
AcidPws <158217569+AcidPws@users.noreply.github.com>
2024-03-16 20:44:04 +0000

    saving 1.2.3...

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,19 @@
 const studentId = 26
+let studentEmail = "Kasturi26@gmail.com"
+var studentPass = "12345"
+studentCity = "Pune"
+let studentState
+
+//studentId = 2 
+//constant variables canot be changed
+
+studentEmail = "928928kasturi@rediff.com"
+studentPass = "09708"
+
+/*
+Prefer not using var!!
+Because of issues related to block scope and functional scope
+*/
+
+console.log(studentId);
+console.table([studentId, studentEmail, studentPass, studentCity, studentState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all JS code as newer version of JS
+
+//alert(5 + 4)  //this is used in  browser not node js
+
+let name = "JavaScript"    //string
+let age = 45               //integer
+let iseasy = true          //boolean
+let stage = null           //null standalone
+let project                //undefined
+let idk = Symbol()         //unique
+
+console.log(typeof idk);   //prints the type of a variable
